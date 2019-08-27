77ae86fe29934d71005df1f6059efc28a915257b
Steven Curtis <stevenpcurtis@hotmail.com>
2019-08-27 16:11:22 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,7 @@
+#  An example App for a folder structure in Xcode
+
+This links to a Medium post [Medium Post] (https://medium.com/@stevenpcurtis.sc/structure-folders-in-xcode-fbb1afa50bc7)
+
+## Installation
+Just click on the green button on GitHub and download into an appropriate location on your local drive. 
+
---
+++ Tidied/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tidied
+//
+//  Created by Steven Curtis on 27/08/2019.
+//  Copyright © 2019 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
